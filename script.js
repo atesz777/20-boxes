31f123fd198211a92201a6d59f1a1091f06534bc
szalerattila <66322285+szalerattila@users.noreply.github.com>
2020-06-08 21:27:58 +0200

    JavaScript

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 let name = "Attila"
 console.log(name)
+
+function printNumbersTill(N) {
+    for (let i = 0; i <= N; i++) {
+        console.log(i)
+}}
+
+printNumbersTill(15);
+
+function getGreetingTo(name) {
+    return 'Hello ' + name + '!'
+}
+
+
+console.log(getGreetingTo('Mark'))
+
+function printValues(array) {
+    
+}
+
+let myNumbers = [10, 20, 30, 40]
+
+myNumbers.forEach((number) => {
+    console.log(number)
+})
